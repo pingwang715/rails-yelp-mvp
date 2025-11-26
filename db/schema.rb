39dfac0e730eec633a0a04fd1c76e3195aeffdb7
Ping Wang <pingwang715@gmail.com>
2025-11-26 11:15:50 +0100

    table data

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_092520) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_26_101115) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
