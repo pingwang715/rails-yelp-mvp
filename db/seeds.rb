e9145ec21d3f99046cae055528f1ba7736d535af
Ping Wang <pingwang715@gmail.com>
2025-11-26 11:24:37 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(name: "Le Petit Chef", address: "Rosenheimer Str. 15, 81667 München", category: "french")
+puts "Created Le Petit Chef"
+puts "Creating restaurants..."
+Restaurant.create!(name: "that's Amore", address: "Ismaninger Str. 116, 81675 München", category: "italian")
+puts "Created that's Amore"
+puts "Creating restaurants..."
+Restaurant.create!(name: "Jiao Kitchen", address: "Arabellastraße 19, 81925 München", category: "chinese")
+puts "Created Jiao Kitchen"
+puts "Creating restaurants..."
+Restaurant.create!(name: "Zento", address: "Oskar-von-Miller-Ring 36, 80333 München", category: "japanese")
+puts "Created Zento"
+puts "Creating restaurants..."
+Restaurant.create!(name: "Le Stollberg", address: "Stollbergstraße 2, 80539 München", category: "french")
+puts "Created Le Stollberg"
+
+puts "Finished! Created #{Restaurant.count} restaurants."
