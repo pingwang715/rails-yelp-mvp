735df29be74e91a6fb1774b06bbb3b40651f22a4
Ping Wang <pingwang715@gmail.com>
2025-11-26 22:07:43 +0100

    Finished Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
 end
+
+# Prefix Verb   URI Pattern                     Controller#Action
+#     restaurants GET    /restaurants(.:format)          restaurants#index
+#                 POST   /restaurants(.:format)          restaurants#create
+#  new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#      restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                 PATCH  /restaurants/:id(.:format)      restaurants#update
+#                 PUT    /restaurants/:id(.:format)      restaurants#update
+#                 DELETE /restaurants/:id(.:format)      restaurants#destroy
+#  Prefix Verb URI Pattern                                       Controller#Action
+#    restaurant_reviews POST /restaurants/:restaurant_id/reviews(.:format)     reviews#create
+# new_restaurant_review GET  /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
